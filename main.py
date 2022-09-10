6b58cfeb3effac14d4f17282b83d814648f05cbf
maom <maom@maoms-MacBook-Pro.local>
2022-09-09 23:08:54 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+
+
+if __name__ == '__main__':
+    # for image in inputs:
+    #     read image
+    #     process image
+    #     print block infomration array
+    #     write output to output folder
